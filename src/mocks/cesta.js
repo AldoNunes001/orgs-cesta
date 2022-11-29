eaf9b46b75b84ace3926cfc10a32c479687eaa4c
Aldo Nunes <aldonunes001@gmail.com>
2022-11-29 14:19:52 -0300

    Editando botao

---
+++ src/mocks/cesta.js
@@ -0,0 +1,17 @@
+import logo from '../../assets/logo.png'
+
+const cesta = {
+    topo: {
+        titulo: 'Detalhes da cesta'
+    },
+    detalhes: {
+        nome: 'Cesta de Verduras',
+        logoFazenda: logo,
+        nomeFazenda: 'Fazenda Bora Bill',
+        descricao: 'Uma cesta com produtos selecionados cuidadosamente da fazenda para a sua cozinha.',
+        preco: 'R$ 40,00',
+        botao: 'Comprar'
+    }
+}
+
+export default cesta
